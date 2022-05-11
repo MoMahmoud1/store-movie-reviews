ef2443a730a81988802dc30b85dea09fa676b419
mohamed <mohamed.baioumy1@outlook.com>
2022-05-11 20:59:57 -0300

    import browser router

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
-import image from "./1.jpg";
+
 import React from "react";
-import { Routes, Route } from "react-router-dom";
 import { Home, Reviews } from './pages';
-
-function Header(props){
-  return(
-    <header>
-      <div id ="header">
-      <h1>{props.name} Movies Reviews</h1>
-      </div>
-    </header>
-  );
-}
+import { Routes, Route } from "react-router-dom";
 
 
-function Main(props){
-  return(
-    <main>
-      <div id="head">
-      <p>The Most {props.adjective} Movies Reviews Wepsite</p>
-      </div>
-      <img src={image} alt="site logo "/>
-      <ul style={{textAlign: "left"}}>
-        {props.movies.map((moive)=>(
-            <li key={moive.id}> {moive.title}</li>
-
-        ))}
-      </ul>
-    </main>
-  );
-}
+// function Header(props){
+//   return(
+//     <header>
+//       <div id ="header">
+//       <h1>{props.name} Movies Reviews</h1>
+//       </div>
+//     </header>
+//   );
+// }
 
 
-function Footer(props){
-  return(
-    <footer>
-      <p>CopyRight {props.year}</p>
-    </footer>
-  );
-}
-const movies =[
-  "moive1",
-  "movie2",
-  "movie3"
-];
-const moiveObject = movies.map((moive, i) =>({id: i, title: moive}));
+// function Main(props){
+//   return(
+//     <main>
+//       <div id="head">
+//       <p>The Most {props.adjective} Movies Reviews Wepsite</p>
+//       </div>
+//       <img src={image} alt="site logo "/>
+//       <ul style={{textAlign: "left"}}>
+//         {props.movies.map((moive)=>(
+//             <li key={moive.id}> {moive.title}</li>
+
+//         ))}
+//       </ul>
+//     </main>
+//   );
+// }
+
+
+// function Footer(props){
+//   return(
+//     <footer>
+//       <p>CopyRight {props.year}</p>
+//     </footer>
+//   );
+// }
+// const movies =[
+//   "moive1",
+//   "movie2",
+//   "movie3"
+// ];
+// const moiveObject = movies.map((moive, i) =>({id: i, title: moive}));
 
 // function Links (){
 //   return(
@@ -62,17 +63,26 @@ const moiveObject = movies.map((moive, i) =>({id: i, title: moive}));
 
 function App() {
   return (
-    <div className="App">
-      <Header name="MO"/>
-      <Main adjective = "Amazing" movies = {moiveObject}/>
-      <div>
-    <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/Reviews" element={<Reviews />} />
-    </Routes>
-  </div>
-      <Footer year = {new Date().getFullYear()} />
+  //   <div className="App">
+  //     <Header name="MO"/>
+  //     <Main adjective = "Amazing" movies = {moiveObject}/>
+  //     <div>
+  //   <Routes>
+  //       <Route path="/" element={<Home />}/>
+  //       <Route path="/Reviews" element={<Reviews />} />
+  //   </Routes>
+  // </div>
+  //     <Footer year = {new Date().getFullYear()} />
+  //   </div>
+  
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reviews" element={<Reviews />} />
+      </Routes>
     </div>
+
+
   );
 
 }
--- src/pages.js
+++ src/pages.js
@@ -1,24 +1,74 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import image from "./1.jpg";
+
+// function Header(props){
+//     return(
+//       <header>
+//         <div id ="header">
+//         <h1>{props.name} Movies Reviews</h1>
+//         </div>
+//       </header>
+//     );
+//   }
+  
+  
+//   function Main(props){
+//     return(
+//       <main>
+//         <div id="head">
+//         <p>The Most {props.adjective} Movies Reviews Wepsite</p>
+//         </div>
+//         <img src={image} alt="site logo "/>
+//         <ul style={{textAlign: "left"}}>
+//           {props.movies.map((moive)=>(
+//               <li key={moive.id}> {moive.title}</li>
+  
+//           ))}
+//         </ul>
+//       </main>
+//     );
+//   }
+  
+  
+//   function Footer(props){
+//     return(
+//       <footer>
+//         <p>CopyRight {props.year}</p>
+//       </footer>
+//     );
+//   }
+//   const movies =[
+//     "moive1",
+//     "movie2",
+//     "movie3"
+//   ];
+//   const moiveObject = movies.map((moive, i) =>({id: i, title: moive}));
+  
+
+
 
 
 export function Home(){
     return(
         <div>
-            <nav>
-                <Link to="movie reviews ">Home</Link>
-                <Link to = "submit movie reviews"> Reviews</Link>
-            </nav>
+       {/* <Header name="MO"/>
+       <Main adjective = "Amazing" movies = {moiveObject}/>
+       <Footer year = {new Date().getFullYear()} /> */}
+        <nav>
+            <Link to = "reviews"> Reviews</Link>
+        </nav>
         </div>
 
-    )
+    );
 }
 export function Reviews(){
     return(
         <div>
             <nav>
-                <Link to="movie reviews ">Home</Link>
-                <Link to = "submit movie reviews"> Reviews</Link>
+                {/* <Link to="movie reviews ">Home</Link>
+                <Link to = "submit movie reviews"> Reviews</Link> */}
+                <h1>Reviews</h1>
             </nav>
         </div>
 
