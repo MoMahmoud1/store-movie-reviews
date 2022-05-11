ca0c7ff26bad712c34f49cd71296a7c550eed647
mohamed <mohamed.baioumy1@outlook.com>
2022-05-11 11:34:29 -0300

    add title and header and logo and movies list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import image from "./1.jpg";
+
+function Header(props){
+  return(
+    <header>
+      <div id ="header">
+      <h1>{props.name} Movies Reviews</h1>
+      </div>
+    </header>
+  );
+}
+
+
+function Main(props){
+  return(
+    <main>
+      <div id="head">
+      <p>The most {props.adjective} Movies Reviews Wepsite</p>
+      </div>
+      <img src={image} alt="site logo "/>
+      <ul style={{textAlign: "left"}}>
+        {props.movies.map((moive)=>(
+            <li key={moive.id}> {moive.title}</li>
+
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+
+function Footer(props){
+  return(
+    <footer>
+      <p>CopyRight {props.year}</p>
+    </footer>
+  );
+}
+const movies =[
+  "moive1",
+  "movie2",
+  "movie3"
+];
+const moiveObject = movies.map((moive, i) =>({id: i, title: moive}));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="MO"/>
+      <Main adjective = "amazing" movies = {moiveObject}/>
+      <Footer year = {new Date().getFullYear()} />
+      
     </div>
   );
 }
