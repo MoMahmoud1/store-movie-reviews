ec83743eefb2d118a5cb61ba3a79a69accd23310
mohamed <mohamed.baioumy1@outlook.com>
2022-05-11 21:14:15 -0300

    add link to reviews page

--- src/pages.js
+++ src/pages.js
@@ -2,48 +2,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 import image from "./1.jpg";
 
-// function Header(props){
-//     return(
-//       <header>
-//         <div id ="header">
-//         <h1>{props.name} Movies Reviews</h1>
-//         </div>
-//       </header>
-//     );
-//   }
+function Header(props){
+    return(
+      <header>
+        <div id ="header">
+        <h1>{props.name} Movies Reviews</h1>
+        </div>
+      </header>
+    );
+  }
   
   
-//   function Main(props){
-//     return(
-//       <main>
-//         <div id="head">
-//         <p>The Most {props.adjective} Movies Reviews Wepsite</p>
-//         </div>
-//         <img src={image} alt="site logo "/>
-//         <ul style={{textAlign: "left"}}>
-//           {props.movies.map((moive)=>(
-//               <li key={moive.id}> {moive.title}</li>
+  function Main(props){
+    return(
+      <main>
+        <div id="head">
+        <p>The Most {props.adjective} Movies Reviews Wepsite</p>
+        </div>
+        <img src={image} alt="site logo "/>
+        <ul style={{textAlign: "left"}}>
+          {props.movies.map((moive)=>(
+              <li key={moive.id}> {moive.title}</li>
   
-//           ))}
-//         </ul>
-//       </main>
-//     );
-//   }
+          ))}
+        </ul>
+      </main>
+    );
+  }
   
   
-//   function Footer(props){
-//     return(
-//       <footer>
-//         <p>CopyRight {props.year}</p>
-//       </footer>
-//     );
-//   }
-//   const movies =[
-//     "moive1",
-//     "movie2",
-//     "movie3"
-//   ];
-//   const moiveObject = movies.map((moive, i) =>({id: i, title: moive}));
+  function Footer(props){
+    return(
+      <footer>
+        <p>CopyRight {props.year}</p>
+      </footer>
+    );
+  }
+  const movies =[
+    "moive1",
+    "movie2",
+    "movie3"
+  ];
+  const moiveObject = movies.map((moive, i) =>({id: i, title: moive}));
   
 
 
@@ -51,13 +51,15 @@ import image from "./1.jpg";
 
 export function Home(){
     return(
-        <div>
-       {/* <Header name="MO"/>
-       <Main adjective = "Amazing" movies = {moiveObject}/>
-       <Footer year = {new Date().getFullYear()} /> */}
-        <nav>
+        <div className="App">
+       <Header name="MO"/>
+       <div id="nav">
             <Link to = "reviews"> Reviews</Link>
-        </nav>
+        </div>
+       <Main adjective = "Amazing" movies = {moiveObject}/>
+       
+       <Footer year = {new Date().getFullYear()} />
+     
         </div>
 
     );
@@ -66,8 +68,7 @@ export function Reviews(){
     return(
         <div>
             <nav>
-                {/* <Link to="movie reviews ">Home</Link>
-                <Link to = "submit movie reviews"> Reviews</Link> */}
+                <Link to="/">Home</Link>
                 <h1>Reviews</h1>
             </nav>
         </div>
